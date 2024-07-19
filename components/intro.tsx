670f572d9c26588304da51f9cc6dc309b2997dee
Suhas H Acharya <suhashacharya@Suhass-MacBook-Air.local>
2024-07-19 00:07:18 -0700

    favicon changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         <span className="font-bold">Frontend Developer</span> with{" "}
         <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Next.js & React Native</span>.
       </motion.h1>
 
       <motion.div
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/SuhasAcharyaCV.pdf"
           download
         >
           Download CV{" "}
